f3ff46cdc9955f7fc338f49203c7fc7f177c5b4e
[Alex] <aedoler@gmail.com>
2016-04-01 09:42:36 -0300

    update

--- football_stats.py
+++ football_stats.py
@@ -2,30 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
-import json
 from bs4 import BeautifulSoup
 
 url = "http://www.cbssports.com/nfl/stats/playersort/nfl/year-2015-season-regular-category-touchdowns"
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page.read())
 
-# print soup.prettify()
 
-#final_link = soup.p.a
-# final_link.decompose()
 
 trs = soup.find_all('tr')
 
-
-
-
 for tr in trs:
-    tds = tr.find_all('td')
+    for link in tr.find_all('a'):
+        fulllink = link.get ('href')
+
+
+    tds = tr.find_all("td")
+
+    try: #we are using "try" because the table is not well formatted. This allows the program to continue after encountering an error.
+        names = str(tds[0].get_text()) # This structure isolate the item by its column in the table and converts it into a string.
+        years = str(tds[1].get_text())
+        positions = str(tds[2].get_text())
+        parties = str(tds[3].get_text())
+        states = str(tds[4].get_text())
+        congress = tds[5].get_text()
+
+    except:
+        continue
+
+    print "Name: {} | Position: {} | Team: {} | Touchdowns {}.".format(names, years, positions, congress)
+"""
+
+
+trs = table.find_all('tr')
+for tr in trs:
+
+    tds = tr.findAll('td')
+    print tds
 
     try:
         names = str(tds[0].get_text())
-        pos = str(tds[1].get_text())
+        test = str(tds[1].get_text())
 
     except:
         print "Bad tr string"
-    print names
+    print names"""
